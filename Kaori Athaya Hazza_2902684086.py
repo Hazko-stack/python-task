9f68222609edf52fce0e6627470d9fc3f21fe5cd
Kaori <hazzakao@gmail.com>
2025-09-22 07:39:26 +0700

    first commit

---
+++ Kaori Athaya Hazza_2902684086.py
@@ -0,0 +1,57 @@
+# soal 1
+alas = float(input("Masukkan alas: "))
+tinggi = float(input("Masukkan tinggi: "))
+
+luas= 0.5 * alas * tinggi
+
+print("Luas segitiga:", luas)
+
+# soal 2
+second = int(input("Masukkan detik: "))
+
+hour = second // 3600
+minute = (second % 3600) // 60
+
+print("Jam:", hour)
+print("Menit:", minute)
+
+# soal 3
+phi = 3.14
+r = float(input("jari-jari: "))
+
+C = 2 * phi * r
+
+print("Keliling lingkaran:", C)
+
+# soal 4
+panjang = float(input("Masukkan panjang: "))
+lebar = float(input("Masukkan lebar: "))
+
+C = (panjang + lebar) * 2
+
+print("Keliling persegi panjang:", C)
+
+# soal 5
+A1 = int(input("Angka pertama: "))
+A2 = int(input("Angka kedua: "))
+A3 = int(input("Angka ketiga: "))
+
+if A1 >= A2 and A1 >= A3:
+    max_val = A1
+elif A2 >= A1 and A2 >= A3:
+    max_val = A2
+else:
+    max_val = A3
+
+if A1 <= A2 and A1 <= A3:
+    min_val = A1
+elif A2 <= A1 and A2 <= A3:
+    min_val = A2
+else:
+    min_val = A3
+
+average = (A1 + A2 + A3) / 3
+
+print("Nilai maksimum:", max_val)
+print("Nilai minimum:", min_val)
+print("Rata-rata:", average)
---
+++ session2.py
@@ -0,0 +1,21 @@
+#string
+nama = "Kaori"
+print("Nama saya:", nama)
+print(nama)
+
+# integer
+umur = 18
+print("Umur saya:", umur)
+print("Hello, nama:", nama)
+
+# float
+tinggi = 170.5
+print("Tinggi badan saya:", tinggi, "cm")
+
+# mantap
+
+a = 10
+b = 5
+c = "%"
+
+print((a*b), c) 
